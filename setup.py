012e16486d1e78dca5c4b05c4834bd9c72b7a3c5
bluesk1 <58339865+bluesk1@users.noreply.github.com>
2020-08-19 15:17:40 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE/"README.md").read_text()
+
+setup(
+   name="REACTColor", 
+   version="0.0.1", 
+   descp="REACTColor can be used to monitor colorimetric reactions.", 
+   long_descp=README,
+   long_descp_content="text/markdown", 
+   author="Saathvik Kannan", 
+   license="MIT", 
+   classifiers=[ 
+        "License :: OSI Approved :: MIT License", 
+        "Programming Language :: Python :: 3", 
+        "Programming Language :: Python :: 3.7", 
+   ], 
+   packages=["REACTColor"], 
+   includepackagedata=True, 
+   installrequires=["pandas","plotly", "Pillow", "opencv-python", "PySimpleGUIWeb","progress"], 
+   entrypoints={ 
+       "console_scripts":[ 
+           "REACTColor=REACTColor.__main__:main", 
+       ] 
+   }, 
+ ) 
